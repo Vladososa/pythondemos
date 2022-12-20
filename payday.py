d8d09b2c7e10cb8571b3049774cb821a67ec29d3
Vladososa <120669035+Vladososa@users.noreply.github.com>
2022-12-20 20:52:08 +0200

    Add files via upload

---
+++ payday.py
@@ -0,0 +1,10 @@
+print("Your name is: ")
+name = str(input())
+print("Hello, " + name + "!")
+
+print("How much hours you worked: ")
+hour = int(input())
+print("Your rate is: ")
+rate = float(input())
+(payday) = hour*rate
+print("Your payday is: " + str(payday))
