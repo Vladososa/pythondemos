5f33b93f0fa37f779cffd52df9f2b1c009de6153
Vladososa <120669035+Vladososa@users.noreply.github.com>
2022-12-20 19:01:19 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,58 @@
+# plus
+def add(x, y):
+    return x + y
+
+# minus
+
+
+def subtract(x, y):
+    return x - y
+
+# umnozhi
+
+
+def multiply(x, y):
+    return x * y
+
+# deleniee
+
+
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # vzimam vhoda
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # burza proverka kakvo sum vzel
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+        # iskam li pak da smqtam
+        # chupi ako ne iskam
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+            break
+
+    else:
+        print("Invalid Input")
